31f58fbb3be1040f58e74da0cc903d46dd39289d
DevWithEasy <devwitheasy@gmail.com>
2025-01-27 11:20:35 +0600

    native spash screen add

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,11 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:mumin/app/providers/ReadProvider.dart';
-import 'package:mumin/app/screens/splash.dart';
+import 'package:mumin/app/screens/main.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
-class MuminApp extends StatelessWidget {
+class MuminApp extends StatefulWidget {
   const MuminApp({super.key});
+
+  @override
+  State<MuminApp> createState() => _MuminAppState();
+}
+
+class _MuminAppState extends State<MuminApp> {
+  @override
+  void initState() {
+    super.initState();
+    initialization();
+  }
+
+  void initialization() async{
+    await Future.delayed(Duration(seconds: 3));
+    FlutterNativeSplash.remove();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +36,7 @@ class MuminApp extends StatelessWidget {
             theme: ThemeData(
                 primarySwatch: Colors.blue,
                 textTheme: GoogleFonts.notoSerifBengaliTextTheme()),
-            home: SplashScreen(),
+            home: MainScreen(),
           );
         }));
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:mumin/app/app.dart';
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const MuminApp());
 }
